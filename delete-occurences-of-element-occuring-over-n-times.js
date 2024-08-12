fa9dbdb724ac037ed8189d80136341b71aea2b9d
dr-frankiestein <150306010+dr-frankiestein@users.noreply.github.com>
2024-08-12 12:54:10 -0400

    Add today's solution

---
+++ delete-occurences-of-element-occuring-over-n-times.js
@@ -0,0 +1,21 @@
+function deleteNth(arr,n){
+  // ...Given a list and a number, 
+  // create a new list that contains each number of list at most N times, 
+  // without reordering.
+ 
+  // create object to store count of each element of og array
+  let freq = {}
+  // create new array for list to return
+  let newList = []
+  // loop over each element of og array
+  for (let i = 0; i <arr.length; i++) {
+    // if not in freq count obj, add element and set count to 0
+    if (!freq[arr[i]]) freq[arr[i]] = 0
+    // add one to count
+    freq[arr[i]]++
+    // if count <= n add to new list
+    if (freq[arr[i]] <= n) newList.push(arr[i])
+  }
+
+  return(newList)
+}
