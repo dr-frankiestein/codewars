8d41c6eeab59f9be033a4ba1fca608b2e56c1f4e
dr-frankiestein <150306010+dr-frankiestein@users.noreply.github.com>
2024-08-19 16:08:20 -0400

    Add today's solution

---
+++ replace-with-alphabet-position.js
@@ -0,0 +1,9 @@
+function alphabetPosition(text) {
+  text = text
+    .toLowerCase()
+    .split('')
+    .map((i)=>i.charCodeAt(0)-96)
+    .filter(i => i>0 && i<=26)
+    .join(' ')
+  return text;
+}
