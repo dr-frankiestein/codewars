5639c0b7ff76e558431904eb19aeca3a4d20bd0a
dr-frankiestein <150306010+dr-frankiestein@users.noreply.github.com>
2024-09-05 10:28:36 -0400

    fixed formatting

--- beginner-num3-sum-of-numbers.js
+++ beginner-num3-sum-of-numbers.js
@@ -1,12 +1,6 @@
 function getSum(a, b) {
-  //   if (a == b) return a;
   let [low, high] = [a, b].sort((a, b) => a - b);
   let sum = 0;
   while (low <= high) sum += low++;
   return sum;
 }
-
-console.log(getSum(599, 164));
-
-console.log(getSum(5, -1));
-console.log(getSum(6, 6));
